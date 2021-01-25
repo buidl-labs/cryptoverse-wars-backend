db1fcc79fd70fc86a3adb3b2a60d0a6e769fa1db
Manan Gouhari <gouhari21@hotmail.com>
2021-01-25 19:04:23 +0530

    Hid the magic key

--- magic.js
+++ magic.js
@@ -1,5 +1,8 @@
-const { Magic } = require('@magic-sdk/admin');
- 
-const magic = new Magic('sk_test_D247CB3A8247F971');
+const { Magic } = require("@magic-sdk/admin");
+
+require("dotenv").config();
+
+const magic = new Magic(process.env.MAGIC_SK);
+const magic = new Magic("sk_test_D247CB3A8247F971");
 
 module.exports = magic;
