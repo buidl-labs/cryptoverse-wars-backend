a89a907f2b4157e2dcb611f10c1072017e4e45ae
Manan Gouhari <gouhari21@hotmail.com>
2021-01-26 02:11:04 +0530

    feat: configured sequelize to use a db connection url from env vars.

--- models/index.js
+++ models/index.js
@@ -1,23 +1,30 @@
 "use strict";
-
+require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
+// const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-	sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// if (config.use_env_variable) {
+// sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+// 	sequelize = new Sequelize(
+// 		config.database,
+// 		config.username,
+// 		config.password,
+// 		config
+// 	);
+// }
+
+const node_env = process.env.NODE_ENV || "development";
+if (node_env === "development") {
+	sequelize = new Sequelize(process.env.DB_URL_DEV);
 } else {
-	sequelize = new Sequelize(
-		config.database,
-		config.username,
-		config.password,
-		config
-	);
+	sequelize = new Sequelize(process.env.DB_URL_PROD, { logging: true });
 }
 
 fs.readdirSync(__dirname)
